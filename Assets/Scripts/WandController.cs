1301206ab5461a124c902107c3f1adee1ae870e7
Robert <bobby@vynyl.com>
2017-09-30 15:12:13 -0600

    initial controller grab script

---
+++ Assets/Scripts/WandController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandController : MonoBehaviour
+{
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+    // Use this for initialization
+    void Start()
+    {
+        this.trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.GetPressDown(triggerButton))
+        {
+
+        }
+        if (controller.GetPressUp(triggerButton))
+        {
+
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+
+    }
+}
