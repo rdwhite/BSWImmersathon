025f1379f5402af8044f4101f981b492f54c866d
Robert <bobby@vynyl.com>
2017-09-30 13:20:50 -0600

    Addition of tent prefabs and scripts to link and plant

--- Assets/Scripts/DrawColliders.cs
+++ Assets/Scripts/DrawColliders.cs
@@ -23,15 +23,15 @@ public class DrawColliders : MonoBehaviour
         // Draw BoxColliders
         foreach (var collider in colliders)
         {
-            Vector3 tl = new Vector3(t.position.x - (b.size.x / 2), t.position.y + (b.size.y / 2), 0f);
-            Vector3 bl = new Vector3(t.position.x - (b.size.x / 2), t.position.y - (b.size.y / 2), 0f);
-            Vector3 br = new Vector3(t.position.x + (b.size.x / 2), t.position.y - (b.size.y / 2), 0f);
-            Vector3 tr = new Vector3(t.position.x + (b.size.x / 2), t.position.y + (b.size.y / 2), 0f);
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(tl, bl);
-            Gizmos.DrawLine(bl, br);
-            Gizmos.DrawLine(br, tr);
-            Gizmos.DrawLine(tr, tl);
+            //Vector3 tl = new Vector3(t.position.x - (b.size.x / 2), t.position.y + (b.size.y / 2), 0f);
+            //Vector3 bl = new Vector3(t.position.x - (b.size.x / 2), t.position.y - (b.size.y / 2), 0f);
+            //Vector3 br = new Vector3(t.position.x + (b.size.x / 2), t.position.y - (b.size.y / 2), 0f);
+            //Vector3 tr = new Vector3(t.position.x + (b.size.x / 2), t.position.y + (b.size.y / 2), 0f);
+            //Gizmos.color = Color.red;
+            //Gizmos.DrawLine(tl, bl);
+            //Gizmos.DrawLine(bl, br);
+            //Gizmos.DrawLine(br, tr);
+            //Gizmos.DrawLine(tr, tl);
         }
     }
 }
---
+++ Assets/Scripts/LinkRigidBody.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void OnRigidBodyLinkHandler(object sender);
+
+[RequireComponent(typeof(Rigidbody))]
+public class LinkRigidBody : MonoBehaviour
+{
+
+    public Rigidbody parentBody;
+    private Rigidbody selfBody;
+
+    public event OnRigidBodyLinkHandler OnLink;
+    public event OnRigidBodyLinkHandler OnUnlink;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.selfBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (parentBody != null)
+        {
+            this.selfBody.isKinematic = true;
+            this.transform.parent = parentBody.transform;
+        }
+    }
+}
