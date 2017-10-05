915259ee1d5772387503e394ad996718fc75ec16
Jordan Clifford <jordan@jfclifford.com>
2017-10-05 14:05:54 -0600

    syncscript

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -5,96 +5,87 @@ using UnityEngine.Networking;
 public class GameLogic : NetworkBehaviour
 {
 
-    public GameObject player;
-    public GameObject eventSystem;
-    public GameObject startUI, restartUI;
+	public GameObject player;
+	public GameObject eventSystem;
+	public GameObject startUI, restartUI;
+	public GameObject directionalLight;
 
-    public float targetTime = 600.0f;
-    public bool ready = false;
-
-
-    public float XRotateAmount = 1f;
-    public float YRotateAmount = 1f;
-    public float ZRotateAmount = 1f;
-
-    private Quaternion initialRotation;
-    private bool shouldRotate;
-    private bool restoreRotation = false;
-
-    void Start()
-    {
-        this.initialRotation = this.transform.rotation;
-
-    }
-
-    private void Instance_TimerStart(int timeLeft)
-    {
-        this.shouldRotate = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (ready == true)
-        {
-
-            if (this.shouldRotate)
-            {
-                this.transform.Rotate(new Vector3(XRotateAmount, YRotateAmount, ZRotateAmount) * Time.deltaTime);
-            }
-            else if (this.restoreRotation)
-            {
-                if (this.transform.rotation == initialRotation)
-                {
-                    this.restoreRotation = false;
-                }
-                else
-                {
-                    this.transform.rotation = Quaternion.Lerp(transform.rotation, initialRotation, 10.0f);
-                }
-            }
+	public float targetTime = 600.0f;
+	public bool ready = false;
 
 
-            if (targetTime <= 0.0f)
-            {
-                timerEnded();
-            }
-        }
+	public float XRotateAmount = 1f;
+	public float YRotateAmount = 1f;
+	public float ZRotateAmount = 1f;
 
-    }
+	private Quaternion initialRotation;
+	private bool shouldRotate;
+	private bool restoreRotation = false;
+
+	void Start()
+	{
+		this.initialRotation = this.transform.rotation;
+
+	}
+
+	private void Instance_TimerStart(int timeLeft)
+	{
+		this.shouldRotate = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (ready == true)
+		{
+
+			if (directionalLight)
+			{
+				directionalLight.transform.Rotate(new Vector3(XRotateAmount, YRotateAmount, ZRotateAmount) * Time.deltaTime);
+			}
 
 
-    public void startPuzzle()
-    { //Begin the puzzle sequence
-        startUI.SetActive(false);
-        targetTime -= Time.deltaTime;
-        ready = true;
-        Debug.Log("foo");
 
-    }
+			if (targetTime <= 0.0f)
+			{
+				timerEnded();
+			}
+		}
+
+	}
+
+
+	public void startPuzzle()
+	{ //Begin the puzzle sequence
+		startUI.SetActive(false);
+		targetTime -= Time.deltaTime;
+		ready = true;
+		Debug.Log("foo");
+
+	}
 
 
 
 
-    public void resetGame()
-    {
-        restartUI.SetActive(false);
-        startUI.SetActive(true);
-        NetworkManager.singleton.ServerChangeScene("main");
-    }
+	public void resetGame()
+	{
+		restartUI.SetActive(false);
+		startUI.SetActive(true);
+		NetworkManager.singleton.ServerChangeScene("main");
+	}
 
 
-    public void gameOver()
-    {
-        restartUI.SetActive(true);
+	public void gameOver()
+	{
+		restartUI.SetActive(true);
 
-    }
+	}
 
-    void timerEnded()
-    {
-        restartUI.SetActive(true);
-        ready = false;
-        //do your stuff here.
-    }
+	void timerEnded()
+	{
+		restartUI.SetActive(true);
+		ready = false;
+		//do your stuff here.
+	}
 
 }
--- Assets/Scripts/spawnscript.cs
+++ Assets/Scripts/spawnscript.cs
@@ -4,33 +4,60 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class spawnscript : NetworkBehaviour {
+	public GameObject rightContSource;
+
+	public GameObject leftContSource;
+
+	public GameObject headObjSource;
+
+	public GameObject ipadcamSource;
 
 	public GameObject tentPrefab;
 	public GameObject ipadCamPrefab;
+	public GameObject headPrefab;
+	public GameObject lefthandPrefab;
+	public GameObject righthandPrefab;
+	public float sendInterval = .01f;
 	// Use this for initialization
-
+	GameObject ipadcam;
+	GameObject head;
+	GameObject lefthand;
+	GameObject righthand;
 
 	public override void OnStartServer() {
-		var ipadcam = (GameObject)Instantiate(ipadCamPrefab, transform.position, transform.rotation);
-
+		ipadcam = (GameObject)Instantiate(ipadCamPrefab);
+		head = (GameObject)Instantiate(headPrefab);
+		lefthand = (GameObject)Instantiate(lefthandPrefab);
+		righthand = (GameObject)Instantiate(righthandPrefab);
 		var tent = (GameObject)Instantiate(tentPrefab, transform.position, transform.rotation);
-		NetworkServer.Spawn(tent);
-	}
-	public override void OnStartClient (){
-		var ipadcam = (GameObject)Instantiate(ipadCamPrefab, transform.position, transform.rotation);
+		NetworkServer.Spawn (tent);
 		NetworkServer.Spawn (ipadcam);
+		NetworkServer.Spawn (head);
+		NetworkServer.Spawn (lefthand);
+		NetworkServer.Spawn (righthand);
+	}
+	void update()
+	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
 
+		//sync pos on network
+		CmdControllerPositionSync();
 	}
+	//sync position on VR controller objects so that VR player movemnts/action can be viewd by normal user
+	[Command]
+	public void CmdControllerPositionSync()
+	{
 
-	void Start () {
-		
-	}
-	void Awake () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		head.transform.localRotation = headObjSource.transform.localRotation;
+		head.transform.position = headObjSource.transform.position;
+		lefthand.transform.localRotation = leftContSource.transform.localRotation;
+		righthand.transform.localRotation = rightContSource.transform.localRotation;
+		lefthand.transform.localPosition = leftContSource.transform.position;
+		righthand.transform.localPosition = rightContSource.transform.position;
+		ipadcam.transform.localPosition = ipadcamSource.transform.position;
+		ipadcam.transform.localPosition = ipadcamSource.transform.position;
 	}
 }
