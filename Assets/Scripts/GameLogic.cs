784d7d315da74d347401037850ae3583a7348137
Scarlett Michanco <scarlett@vynyl.com>
2017-10-05 12:53:51 -0600

    countdown

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -8,15 +8,28 @@ public class GameLogic : NetworkBehaviour
     public GameObject player;
     public GameObject eventSystem;
     public GameObject startUI, restartUI;
-  
+
+    public float targetTime = 600.0f;
+    public bool ready = false;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ready == true)
+        {
+            targetTime -= Time.deltaTime;
+
+
+            if (targetTime <= 0.0f)
+            {
+                timerEnded();
+            }
+        }
 
     }
 
@@ -24,27 +37,34 @@ public class GameLogic : NetworkBehaviour
     public void startPuzzle()
     { //Begin the puzzle sequence
         startUI.SetActive(false);
-		restartUI.SetActive (true);
-        
+        restartUI.SetActive(true);
+        ready = true;
         Debug.Log("foo");
 
     }
 
-    
+
 
 
     public void resetGame()
     {
         restartUI.SetActive(false);
         startUI.SetActive(true);
-		NetworkManager.singleton.ServerChangeScene("main");
+        NetworkManager.singleton.ServerChangeScene("main");
     }
 
 
     public void gameOver()
-    { 
+    {
         restartUI.SetActive(true);
 
+    }
+
+    void timerEnded()
+    {
+        restartUI.SetActive(true);
+        ready = false;
+        //do your stuff here.
     }
 
 }
