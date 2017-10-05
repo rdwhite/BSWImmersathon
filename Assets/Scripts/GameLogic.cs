19de5bacaa2f4f55cdac9d51ef5186cc5676824b
Scarlett Michanco <scarlett@vynyl.com>
2017-10-05 12:02:36 -0600

    add gamelogic

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject eventSystem;
+    public GameObject startUI, restartUI;
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void startPuzzle()
+    { //Begin the puzzle sequence
+        startUI.SetActive(false);
+        eventSystem.SetActive(false);
+        Debug.Log("foo");
+
+    }
+
+    
+
+
+    public void resetGame()
+    {
+        restartUI.SetActive(false);
+        startUI.SetActive(true);
+    }
+
+
+    public void gameOver()
+    { 
+        restartUI.SetActive(true);
+
+    }
+
+}
