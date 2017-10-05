b30899bfd26d98f70b1208bae6468449bc95a38c
Jordan Clifford <jordan@jfclifford.com>
2017-10-05 13:06:35 -0600

    spawnscriptupdate

--- Assets/Scripts/spawnscript.cs
+++ Assets/Scripts/spawnscript.cs
@@ -6,16 +6,21 @@ using UnityEngine.Networking;
 public class spawnscript : NetworkBehaviour {
 
 	public GameObject tentPrefab;
+	public GameObject ipadCamPrefab;
 	// Use this for initialization
 
 
 	public override void OnStartServer() {
-		
+		var ipadcam = (GameObject)Instantiate(ipadCamPrefab, transform.position, transform.rotation);
 
 		var tent = (GameObject)Instantiate(tentPrefab, transform.position, transform.rotation);
 		NetworkServer.Spawn(tent);
 	}
+	public override void OnStartClient (){
+		var ipadcam = (GameObject)Instantiate(ipadCamPrefab, transform.position, transform.rotation);
+		NetworkServer.Spawn (ipadcam);
 
+	}
 
 	void Start () {
 		
