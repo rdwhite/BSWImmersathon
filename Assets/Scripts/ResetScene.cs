3cd01d4341fea4fe973732e88d34e7524bb141a5
Scarlett Michanco <scarlett@vynyl.com>
2017-10-05 00:05:59 -0600

    add reset script

---
+++ Assets/Scripts/ResetScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    void OnReset()
+    {
+        if (GUILayout.Button("Reset"))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
+}
